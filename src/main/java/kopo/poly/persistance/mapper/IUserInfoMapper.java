721f742144a95768b9d3306cbdc15275d33682d7
Geuniv <kkoowl99@gmail.com>
2023-07-05 14:09:57 +0900

    2023.07.05 ( 방학 Spring 특강 3일차 로그인 기능 )

--- src/main/java/kopo/poly/persistance/mapper/IUserInfoMapper.java
+++ src/main/java/kopo/poly/persistance/mapper/IUserInfoMapper.java
@@ -13,4 +13,8 @@ public interface IUserInfoMapper {
     UserInfoDTO getLogin(UserInfoDTO pDTO) throws Exception;
 
     // 회원 가입 전 아이디 중복체크하기 ( DB 조회하기 )
+    UserInfoDTO getUserIdExists(UserInfoDTO pDTO) throws Exception;
+
+    // 회원 가입 전 이메일 중복체크하기 ( DB 조회하기 )
+    UserInfoDTO getEmailExists(UserInfoDTO pDTO) throws Exception;
 }
